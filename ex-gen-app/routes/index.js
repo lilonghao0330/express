943b490727742b76e7cc627e0bb5615d79ae5e0c
lilonghao <longhao.li@accenture.com>
2018-11-13 00:28:23 +0900

    Draft完成

--- ex-gen-app/routes/index.js
+++ ex-gen-app/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 
 /* GET home page. */
@@ -8,41 +9,44 @@ router.get('/', function(req, res, next) {
   res.render('index.ejs',
   {title:'じゃんけんで勝負だよ',
    content:msg,
-   link:{herf:"/",text:''},
+   link:{herf:"/result",text:'※決戦へ'}
+  });
+  });
+
+
+router.post('/result',function(req, res, next) {
+  var msg = req.body['select'];
+ var sample = JSON.parse(fs.readFileSync('../ex-gen-app/public/json/sample.json', 'utf8'))
+ console.log(sample);
+ var key =Object.keys(sample);
+ console.log(key);
+ var i =key[Math.floor(Math.random()*key.length)];
+ console.log(i);
+ if(msg == "グー" ) {
+  cpunum = 0;
+} else if(msg == "チョキ") {
+  cpunum = 1;
+} else if(msg == "パー") {
+  cpunum = 2;
+}
+if(cpunum == 0 && i == 1) {
+  judge = "あなたの勝ち";
+} else if(cpunum == 1 && i == 2) {
+  judge = "あなたの勝ち";
+} else if(cpunum == 2 && i == 0) {
+  judge = "あなたの勝ち";
+} else if(cpunum == i) {
+  judge = "ひきわけ";
+} else {
+  judge = "あなたの負け";
+}
+  res.render('result.ejs',
+  {title:'決戦！！',
+  content1:'あなたは「'+ msg +'」を選んだ！',
+  content2:'相手が「'+ sample[i].name + '」を選んだ！',
+  content3:'なんと' + judge +'!',
+   link:{herf:"/",text:'※もう一回'},
   });
 });
 
-//router.post('/',function(req, res, next) {
-  //var msg = req.body['select'];
-  //console.log(msg);
-//});
-
-router.post('/post',function(req, res, next) {
-  //var msg = req.body['select'];
-  //var data={
-    //title:"じゃんけんで勝負だよ",
-    //content:'あなたは「'　+ msg +'」を選んだ、決戦Pageにて結果を見よう',
-    //link:{herf:"/result",text:'※決戦へ'}
-  //};
-  //res.render('index.ejs',data);
-  var response = {
-    "name":req.body['select'],
-   };
-  console.log(response);
-  res.end(JSON.stringify(response));
-  exports.response = response;
-});
-//postpageのFormに結果を反映、
-//決戦リンクでコンテンツを送信
-//router.post('/post/response',function(req, res, next) {
-  //var response = {
-    //"name":req.body['select'],
-  //};
-  //console.log(response);
-  //res.end(JSON.stringify(response));
-  //var data={
-    //content:'あなたは「'　+ select +'」を選んだ、決戦開始を押して！',
-  //};
-  //res.render('index.ejs',data);
-//});
 module.exports = router;
--- ex-gen-app/routes/result.js
+++ ex-gen-app/routes/result.js
@@ -2,27 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET result page. */
-router.get('/', function(req, res, next) {
-    res.render('result.ejs',
-    {title:'決戦！！',
-    //content:'あなたは「」を選んだ、決戦開始を押して！',
-     link:{herf:"/",text:'※もう一回'},
-    });
-    var index =require('./index.js');  
-    console.log(index.response);
-});
-
-router.get('/post',function(req, res, next) {
-    var msg = req.body['select'];
-    console.log(msg);
-  });
-//router.get('/post', function(req, res, next) {
-    //res.render('result.ejs',
-    //{content:'あなたは「'　+ select +'」を選んだ、決戦開始を押して！',
-    //});
-
-//});
 
 
 module.exports = router;
